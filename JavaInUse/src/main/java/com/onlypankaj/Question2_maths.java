3d53009f3959d7fda631be5e4c48e9251ff0c270
onlypankaj <onlypankaj@gmail.com>
2021-06-15 01:46:47 +0530

    Question2_maths

---
+++ JavaInUse/src/main/java/com/onlypankaj/Question2_maths.java
@@ -0,0 +1,8 @@
+package com.onlypankaj;
+
+public class Question2_maths {
+    public static void main(String[] args) {
+        System.out.println(75 / 2.0);
+        System.out.println(75 % 2.0);
+    }
+}
